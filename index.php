82e0e9b423cff9f25af6fd903241231b336b2f30
Joshua Guillery <jbuist@iunu.com>
2019-02-07 10:21:26 -0500

    Revised CSS Links

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <!--
 Author: Joshua Buist
-Luna Visum Monitoring Platform (c) 2019 - All Rights Reserved
-Licensed to iUNU Inc. 2019
+Visum IO Monitoring Platform (c) 2019 - All Rights Reserved
+
 -->
 
-{% load static %}
 <?php 
 
 $panels = 4;
@@ -22,12 +21,12 @@ echo "Hello World";
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" type="text/css" media="screen" href="{% static 'view/css/reset.css' %}?v=1.06" />
-    <link rel="stylesheet" type="text/css" media="screen" href="{% static 'view/css/fonts.css' %}?v=1.06" />
-    <link rel="stylesheet" type="text/css" media="screen" href="{% static 'view/css/colors.css' %}?v=1.06" />
-    <link rel="stylesheet" type="text/css" media="screen" href="{% static 'view/css/main.css' %}?v=1.06" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/reset.css?v=1.06" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/fonts.css?v=1.06" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/colors.css?v=1.06" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main.cssv=1.06" />
     
-    <script src="{% static 'view/js/main.js' %}"></script>
+    <script src="js/main.js"></script>
 </head>
 <body>
     <header>

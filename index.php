9894f0c67816869ffcbff840875aa568844e6e8d
Joshua Guillery <jbuist@iunu.com>
2019-02-07 11:01:38 -0500

    Link Fix

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ $panels = 4;
     <link rel="stylesheet" type="text/css" media="screen" href="css/reset.css?v=1.06" />
     <link rel="stylesheet" type="text/css" media="screen" href="css/fonts.css?v=1.06" />
     <link rel="stylesheet" type="text/css" media="screen" href="css/colors.css?v=1.06" />
-    <link rel="stylesheet" type="text/css" media="screen" href="css/main.cssv=1.06" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css?v=1.06" />
     
     <script src="js/main.js"></script>
 </head>

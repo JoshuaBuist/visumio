a6e2af027902926bd9fa29c790efcc58f630b9f1
jbuist <jbuist@iunu.com>
2019-02-09 18:08:50 -0500

    Auth redirect

--- auth.py
+++ auth.py
@@ -3,3 +3,20 @@
 # TO-DO 
 # Proper authentification
 
+import sys
+import cgi
+import cgitb
+
+cgitb.enable() # Debug Setting
+
+form = cgi.FieldStorage() # Data stored from POST
+
+user = form.getvalue("user")
+password = form.getvalue("pass")
+
+print ('Content-Type: text/html')
+
+if len(user) > 0 and len(password) > 5:
+    print ('Location: http://http://68.183.200.39/index.py')
+else:
+    print ('<h1>Wrong input</h1')

a36506f5d228aa43ec088af0d26747fa2fc2e2e2
Joshua Guillery <jbuist@iunu.com>
2019-02-08 23:48:00 -0500

    Python fix

--- index.py
+++ index.py
@@ -2,10 +2,10 @@
 
 def load_page(url):
     page = "".join(open(url,'r').readlines())
-    print (page)
+    print page
 
 def main():
-    print ('Content-type: text/html\n\n')
+    print 'Content-type: text/html\n\n'
     load_page("templates/app.html")
 
 if __name__ == "__main__":

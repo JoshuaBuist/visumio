45910edf334bf56455ce8776fb23b239041f33d8
Joshua Guillery <jbuist@iunu.com>
2019-02-09 00:23:03 -0500

    Python CGI Fix

--- index.py
+++ index.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python
 
-print ('Content-type: text/html\n\n')
-
-def load_page(url):
-
-    page = "".join(open(url,'r').readlines())
-
-    print (page)
-
-def main():
-    load_page("templates/app.html")
-
-if __name__ == "__main__":
-    main()
+html = """Content-type: text/html\n
+ 
+<html>
+<head>
+<title>Python Porting Guide</title>
+{MENU}
+</head>
+<body>
+<br>
+Input all or a portion of the known syntax for either Python2.x or Python3.x <br />
+Results are based only on version differences, not every module in existence to python<br />
+ 
+<form method=POST action='portingguide.py'>
+        <p><input type=text name=search placeholder="'viewall' for everything">
+        <p><input type=submit value='Search'> <!-- <input type="button" onclick="change.viewall()"        value="viewall"> -->
+<br /><br />
+{SEARCHED_TEXT}{SEARCHED}<br /><br />
+{SEARCH_TEXT} <br /><br />
+</body>
+</html>
+""".format(
+        MENU="Hello",
+        SEARCHED="HELLO",
+        SEARCH_TEXT="NEW",
+        SEARCHED_TEXT="NEW 2",
+        )
+print(html)

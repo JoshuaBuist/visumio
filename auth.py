47310e211bca6aa984746588d99ab5fac2977b88
jbuist <jbuist@iunu.com>
2019-02-09 18:25:00 -0500

    Auth redirect

--- auth.py
+++ auth.py
@@ -14,7 +14,8 @@ form = cgi.FieldStorage() # Data stored from POST
 user = form.getvalue("user")
 password = form.getvalue("pass")
 
-print ('Content-Type: text/html')
+print ('Content-Type: text/html\n\n')
+
 
 if len(user) > 0 and len(password) > 5:
     print ('Location: http://http://68.183.200.39/index.py')

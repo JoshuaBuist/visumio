6e3ecf735934266c74f2fb1ef63f696fe8777c10
jbuist <jbuist@iunu.com>
2019-02-09 14:08:26 -0500

    Improved Templating System

--- index.py
+++ index.py
@@ -1,14 +1,36 @@
 #!/usr/bin/env python
 
+class Template:
+
+    def __init__(self,_view,_handles):
+        self.view = _view
+        self.handles = _handles
+
+    def render(self):
+        print (self.view)
+
+
 def load_page(url):
 
-    page = "".join(open(url,'r').readlines()).format(value="Demo")
+    page = open(url,'r').readlines()
 
-    print (page)
+    return page
 
+
+def render_page(content):
+
+    print ('Content-type: text/html\n\n' + content)
+
+
+# Main
+#
 def main():
-    print ('Content-type: text/html\n\n')
-    load_page("templates/app.html")
+    
+    page = load_page("templates/app.html")
+    render_page(page)
+
+    main_page = Template('templates/app.html',['value','value2'])
+
 
 if __name__ == "__main__":
     main() 

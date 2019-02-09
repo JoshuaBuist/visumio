1aaf5b2ac97bedff19461db138a4af55e3a370db
Joshua Guillery <jbuist@iunu.com>
2019-02-09 00:18:04 -0500

    Python fix

--- index.py
+++ index.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python
 
-#from string import Template
-
 def load_page(url):
 
     page = "".join(open(url,'r').readlines())
 
-    #Template(page).substitute(value="Hello World")
-
-    #if len(__handles) > 0:
-        # Added handles
-
-    print page
+    print (page)
 
 def main():
-    print 'Content-type: text/html\n\n'
+    print ('Content-type: text/html\n\n')
     load_page("templates/app.html")
 
 if __name__ == "__main__":
